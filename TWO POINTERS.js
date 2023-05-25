d49d3e0a29a58248ae107751c965720c7cb57402
siddhart144 <sd15111999@gmail.com>
2023-05-25 12:14:45 +0530

    first commit

--- TWO POINTERS.js
+++ TWO POINTERS.js
@@ -23,20 +23,69 @@
 // console.log(bag);
 
 
-var arr=[12,34,11,9,18];
-var left=0;
-var right=arr.length-1;
+// var arr=[12,34,11,9,18];
+// var left=0;
+// var right=arr.length-1;
 
 
-function swap(arr,right,left){
-   let temp=arr[right]
-   arr[right]=arr[left]
-   arr[left]=temp
+// function swap(arr,right,left){
+//    let temp=arr[right]
+//    arr[right]=arr[left]
+//    arr[left]=temp
+// }
+
+// while(left<right){
+//     swap(arr,left,right)
+//     left++;
+//     right--;
+// }
+// console.log(arr);
+
+
+
+
+// var arr=["A","B","A","B","A","B","B"]
+// var bag="";
+// var obj={};
+// var max=-Infinity;
+// for (var i=0;i<arr.length;i++){
+//     var str=arr[i]
+//     if(obj[str]==undefined){
+//         obj[str]=1;
+//     }
+//     else{
+//         obj[str]=obj[str]+1
+//     }
+// }
+
+// for(var key in obj){
+//     if(obj[key]>max){
+//         max=obj[key]
+//         bag=key
+//     }
+// }
+// console.log(bag);
+
+var arr=[5,6,7,8,5,5];
+var obj={}
+var max=-Infinity;
+ var bag="";
+
+for (var i=0;i<arr.length;i++){
+    var char=arr[i]
+   if(obj[char]===undefined){
+      
+          obj[char]=1    
+   }else{
+    obj[char] = obj[char] +1
+   }
 }
 
-while(left<right){
-    swap(arr,left,right)
-    left++;
-    right--;
+console.log(obj);
+for(var key in obj){
+    if(obj[key]>max){
+        max=obj[key]
+        bag=key
+    }
 }
-console.log(arr);
+console.log(bag);
---
+++ anagram.js
@@ -0,0 +1,24 @@
+function anagram(a,b){
+    let x=a.split("").sort().join()
+    let y=b.split("").sort().join()
+     console.log(x,y);
+    if(x===y) {
+        return true 
+    } 
+    else {
+        return false
+    }
+}
+let z= anagram ("hello" , "lloeh")
+console.log(z);
+
+// git remote add origin https://github.com/Siddharth144/basic-dsa.git
+// git branch -M main
+// git push -u origin main
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Siddharth144/basic-dsa.git
+// git push -u origin main
